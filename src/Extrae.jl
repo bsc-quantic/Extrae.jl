17cada66033cf0ffffbebce03fc388649eb3d3d5
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-11-29 12:50:44 +0000

    Format docstrings

--- src/Extrae.jl
+++ src/Extrae.jl
@@ -5,7 +5,9 @@ const Type = FFI.Type
 const Value = FFI.Value
 
 """
-Returns the version of the underlying TRACE package.
+    version()
+
+Return the version of the underlying TRACE package.
 
 Although an application may be compiled to a specific TRACE library, by using the appropiate shared library commands, the application may use a different TRACE library.
 """
@@ -21,7 +23,9 @@ end
 export version
 
 """
-Initializes the tracing library.
+    init()
+
+Initialize the tracing library.
 
 This routine is called automatically in different circumstances, which include:
 - Call to MPI_Init when the appropriate instrumentation library is linked or preload with the application.
@@ -34,41 +38,58 @@ init() = FFI.Extrae_init()
 export init
 
 
-"True if the instrumentation has been initialized and if so, which mechanism was the first to initialize it."
+"""
+    isinitialized()
+
+Return `true` if the instrumentation has been initialized and if so, which mechanism was the first to initialize it.
+"""
 isinitialized() = FFI.Extrae_is_initialized()
 export isinitialized
 
-"Finalizes the tracing library and dumps the intermediate tracing buffers onto disk."
+"""
+    finish()
+
+Finalize the tracing library and dumps the intermediate tracing buffers onto disk.
+"""
 finish() = FFI.Extrae_fini()
 export finish
 
-"Forces the calling thread to write the events stored in the tracing buffers to disk."
+"""
+    flush()
+
+Force the calling thread to write the events stored in the tracing buffers to disk.
+"""
 flush() = FFI.Extrae_flush()
 export flush
 
+"""
+    instrumentation(state)
+
+Turn on/off the instrumentation.
+"""
 instrumentation(state::Bool) = state ? restart() : shutdown()
 export instrumentation
 
 """
-Adds a single timestampted event into the tracefile.
+Add a single timestampted event into the tracefile.
 
 Some common uses of events are:
 - Identify loop iterations (or any code block): Given a loop, the user can set a unique type for the loop and a value related to the iterator value of the loop. For example:
 
-	```c
+  ```c
     for (i = 1; i <= MAX_ITERS; i++)
     {
       Extrae_event (1000, i);
       [original loop code]
       Extrae_event (1000, 0);
     }
-	````
+  ````
 
     The last added call to Extrae_event marks the end of the loop setting the event value to 0, which facilitates the analysis with Paraver.
 
 - Identify user routines: Choosing a constant type (6000019 in this example) and different values for different routines (set to 0 to mark a "leave" event).
 
-	```c
+  ```c
     void routine1 (void)
     {
       Extrae_event (6000019, 1);
@@ -82,7 +103,7 @@ Some common uses of events are:
       [routine 2 code]
       Extrae_event (6000019, 0);
     }
-	```
+  ```
 
 - Identify any point in the application using a unique combination of type and value.
 """
@@ -107,20 +128,38 @@ define_event(type::Type, desc::String, values::Vector{Tuple{Value,String}}) = be
     FFI.Extrae_define_event_type(type, Base.cconvert(desc), nvalues, Ref(_values), Ref(_descs))
 end
 
-"Emits the value of the active hardware counters set."
+"""
+    counters()
+
+Emit the value of the active hardware counters set.
+"""
 counters() = FFI.Extrae_counters()
 
-"Makes the previous hardware counter set defined in the XML file to be the active set."
+"""
+    previous_hwc_set()
+
+Switch to the previous hardware counter set defined in the XML file.
+"""
 previous_hwc_set() = FFI.Extrae_previous_hwc_set()
 
-"Makes the following hardware counter set defined in the XML file to be the active set."
+"""
+    next_hwc_set()
+
+Switch to the following hardware counter set defined in the XML file.
+"""
 next_hwc_set() = FFI.Extrae_next_hwc_set()
 
-"Allows the user to choose from which tasks (not threads!) store information in the tracefile."
+"""
+    set_tracing_tasks()
+
+Select the range of tasks (not threads!) to store information from in the tracefile.
+"""
 set_tracing_tasks(interval::UnitRange{UInt32}) = FFI.Extrae_set_tracing_tasks(interval.start::UInt32, interval.stop::UInt32)
 
 """
-Permits configuring several tracing options at runtime. The options parameter has to be a bitwise or combination of the following options, depending on the user's needs:
+    setoption(options)
+
+Configure tracing options at runtime. The options parameter has to be a bitwise or combination of the following options, depending on the user's needs:
 - EXTRAE_CALLER_OPTION Dumps caller information at each entry or exit point of the MPI routines. Caller levels need to be configured at XML.
 - `EXTRAE_HWC_OPTION` Activates hardware counter gathering.
 - `EXTRAE_MPI_OPTION` Activates tracing of MPI calls.
@@ -131,14 +170,26 @@ Permits configuring several tracing options at runtime. The options parameter ha
 """
 setoption(options::FFI.Options) = FFI.Extrae_set_options(options)
 
-"Emits the value of the network counters if the system has this capability (Only available for systems with Myrinet GM/MX networks)."
+"""
+    network_counters()
+
+Emit the value of the network counters if the system has this capability (Only available for systems with Myrinet GM/MX networks).
+"""
 network_counters() = FFI.Extrae_network_counters()
 
-"Emits the network routes for an specific `task`."
+"""
+    network_routes(task)
+
+Emit the network routes for an specific `task`.
+"""
 network_routes(task) = FFI.Extrae_network_routes(task)
 
 """
-Emits an event into the tracefile which references the source code (data includes: source line number, file name and function name). If enter is 0 it marks an end (i.e., leaving the function), otherwise it marks the beginning of the routine. The user must be careful to place the call of this routine in places where the code is always executed, being careful not to place them inside if and return statements. The function returns the address of the reference.
+    user_function(enter)
+
+Emit an event into the tracefile which references the source code (data includes: source line number, file name and function name).
+
+If enter is 0 it marks an end (i.e., leaving the function), otherwise it marks the beginning of the routine. The user must be careful to place the call of this routine in places where the code is always executed, being careful not to place them inside if and return statements. The function returns the address of the reference.
 ```c
 void routine1 (void)
 {
@@ -157,7 +208,7 @@ void routine2 (void)
 
 In order to gather performance counters during the execution of these calls, the user-functions tag and its counters have to be both enabled int section :ref:`sec:XMLSectionUF`.
 
-Warning
+# Warning
 
 Note that you need to compile your application binary with debugging information (typically the `-g` compiler flag) in order to translate the captured addresses into valuable information such as function name, file name and line number.
 """
