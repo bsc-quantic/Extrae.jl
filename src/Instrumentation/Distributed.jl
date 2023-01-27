618e617316140c2bf9c0c828d4f71bcc082f4abb
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-01-27 17:28:49 +0000

    Emit events on `Distributed` hooks

--- src/Instrumentation/Distributed.jl
+++ src/Instrumentation/Distributed.jl
@@ -1,31 +1,44 @@
 using Distributed
 
+struct DistributedEvent{ValueCode} <: Event{400002,ValueCode} end
+
+const DistributedEnd = DistributedEvent{0}()
+const DistributedAddProcs = DistributedEvent{1}()
+const DistributedRmProcs = DistributedEvent{2}()
+const DistributedInitWorker = DistributedEvent{3}()
+const DistributedStartWorker = DistributedEvent{4}()
+const DistributedRemoteCall = DistributedEvent{5}()
+const DistributedRemoteCallFetch = DistributedEvent{6}()
+const DistributedRemoteCallWait = DistributedEvent{7}()
+const DistributedProcessMessages = DistributedEvent{8}()
+const DistributedInterrupt = DistributedEvent{9}()
+
 # worker management
-Cassette.prehook(::ExtraeCtx, ::typeof(addprocs), args...) = println("[TRACE] addprocs - begin @ $(myid())")
-Cassette.posthook(::ExtraeCtx, _, ::typeof(addprocs), args...) = println("[TRACE] addprocs - end @ $(myid())")
+Cassette.prehook(::ExtraeCtx, ::typeof(addprocs), args...) = emit(DistributedAddProcs)
+Cassette.posthook(::ExtraeCtx, _, ::typeof(addprocs), args...) = emit(DistributedEnd)
 
-Cassette.prehook(::ExtraeCtx, ::typeof(rmprocs), args...) = println("[TRACE] rmprocs - begin @ $(myid())")
-Cassette.posthook(::ExtraeCtx, _, ::typeof(rmprocs), args...) = println("[TRACE] rmprocs - end @ $(myid())")
+Cassette.prehook(::ExtraeCtx, ::typeof(rmprocs), args...) = emit(DistributedRmProcs)
+Cassette.posthook(::ExtraeCtx, _, ::typeof(rmprocs), args...) = emit(DistributedEnd)
 
-Cassette.prehook(::ExtraeCtx, ::typeof(init_worker), args...) = println("[TRACE] init_worker - begin @ $(myid())")
-Cassette.posthook(::ExtraeCtx, _, ::typeof(init_worker), args...) = println("[TRACE] init_worker - end @ $(myid())")
+Cassette.prehook(::ExtraeCtx, ::typeof(init_worker), args...) = emit(DistributedInitWorker)
+Cassette.posthook(::ExtraeCtx, _, ::typeof(init_worker), args...) = emit(DistributedEnd)
 
-Cassette.prehook(::ExtraeCtx, ::typeof(start_worker), args...) = println("[TRACE] start_worker - begin @ $(myid())")
-Cassette.posthook(::ExtraeCtx, _, ::typeof(start_worker), args...) = println("[TRACE] start_worker - end @ $(myid())")
+Cassette.prehook(::ExtraeCtx, ::typeof(start_worker), args...) = emit(DistributedStartWorker)
+Cassette.posthook(::ExtraeCtx, _, ::typeof(start_worker), args...) = emit(DistributedEnd)
 
 # task creation
-Cassette.prehook(::ExtraeCtx, ::typeof(remotecall), f, ::Distributed.Worker, args...) = println("[TRACE] remotecall[$f] - begin @ $(myid())")
-Cassette.posthook(::ExtraeCtx, _, ::typeof(remotecall), f, ::Distributed.Worker, args...) = println("[TRACE] remotecall[$f] - end @ $(myid())")
+Cassette.prehook(::ExtraeCtx, ::typeof(remotecall), f, ::Distributed.Worker, args...) = emit(DistributedRemoteCall)
+Cassette.posthook(::ExtraeCtx, _, ::typeof(remotecall), f, ::Distributed.Worker, args...) = emit(DistributedEnd)
 
-Cassette.prehook(::ExtraeCtx, ::typeof(remotecall_fetch), ::typeof(Distributed.fetch_ref), ::Distributed.Worker, args...) = println("[TRACE] remotecall_fetch - begin @ $(myid())")
-Cassette.posthook(::ExtraeCtx, _, ::typeof(remotecall_fetch), ::typeof(Distributed.fetch_ref), ::Distributed.Worker, args...) = println("[TRACE] remotecall_fetch - end @ $(myid())")
+Cassette.prehook(::ExtraeCtx, ::typeof(remotecall_fetch), ::typeof(Distributed.fetch_ref), ::Distributed.Worker, args...) = emit(DistributedRemoteCallFetch)
+Cassette.posthook(::ExtraeCtx, _, ::typeof(remotecall_fetch), ::typeof(Distributed.fetch_ref), ::Distributed.Worker, args...) = emit(DistributedEnd)
 
-Cassette.prehook(::ExtraeCtx, ::typeof(remotecall_wait), args...) = println("[TRACE] remotecall_wait - begin @ $(myid())")
-Cassette.posthook(::ExtraeCtx, _, ::typeof(remotecall_wait), args...) = println("[TRACE] remotecall_wait - end @ $(myid())")
+Cassette.prehook(::ExtraeCtx, ::typeof(remotecall_wait), args...) = emit(DistributedRemoteCallWait)
+Cassette.posthook(::ExtraeCtx, _, ::typeof(remotecall_wait), args...) = emit(DistributedEnd)
 
 # task management
-Cassette.prehook(::ExtraeCtx, ::typeof(process_messages), args...) = println("[TRACE] process_messages - begin @ $(myid())")
-Cassette.posthook(::ExtraeCtx, _, ::typeof(process_messages), args...) = println("[TRACE] process_messages - end @ $(myid())")
+Cassette.prehook(::ExtraeCtx, ::typeof(process_messages), args...) = emit(DistributedProcessMessages)
+Cassette.posthook(::ExtraeCtx, _, ::typeof(process_messages), args...) = emit(DistributedEnd)
 
-Cassette.prehook(::ExtraeCtx, ::typeof(interrupt), args...) = println("[TRACE] interrupt - begin @ $(myid())")
-Cassette.posthook(::ExtraeCtx, _, ::typeof(interrupt), args...) = println("[TRACE] interrupt - end @ $(myid())")
+Cassette.prehook(::ExtraeCtx, ::typeof(interrupt), args...) = emit(DistributedInterrupt)
+Cassette.posthook(::ExtraeCtx, _, ::typeof(interrupt), args...) = emit(DistributedEnd)
