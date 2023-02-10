4c6cd41a8cedff7ed4fad4a1b847651b948edc65
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-02-10 15:41:03 +0100

    Add `register(::Event)` method

--- src/API.jl
+++ src/API.jl
@@ -87,11 +87,13 @@ end
 emit(events::Vector{Event}; counters::Bool=false) = foreach(e -> event(e; counters=counters), events)
 
 """
+    register(event)
     register(event, description)
     register(events, description)
 
 Document to the Paraver Configuration File human readable information regarding type type and its values values.
 """
+register(::Type{E}) where {E<:Event} = register(E, description(E))
 register(::Type{<:Event{T}}, desc::String) where {T} = FFI.Extrae_define_event_type(T, Base.cconvert(Cstring, desc), 0, Nothing, Nothing)
 function register(events::Vector{<:Event{T,V} where {V}}, desc::String) where {T}
     nvalues = length(events)
