f4a2c0c968a7371b949df294768cdc4f9d3c1d38
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-05-06 17:14:15 +0200

    Set some envs on init

--- src/Extrae.jl
+++ src/Extrae.jl
@@ -5,4 +5,12 @@ include("API.jl")
 export Event, typecode, valuecode, description
 export version, init, isinit, finish, flush, instrumentation, emit, register, previous_hwc_set, next_hwc_set, set_tracing_tasks, setoption, network_counters, network_routes, user_function
 
+function __init__()
+    FFI.Extrae_init()
+
+    get!(ENV, "EXTRAE_HOME", Extrae_jll.artifact_dir)
+    get!(ENV, "EXTRAE_DISABLE_OMP", true)
+    get!(ENV, "EXTRAE_DISABLE_PTHREAD", false)
+end
+
 end
