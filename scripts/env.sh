225a028cb68c0963f3ea150572dedd5510dc49a9
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-05-07 18:28:05 +0200

    Update `env.sh` to use `extrae.xml` configuration

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 export EXTRAE_ON=1
 export EXTRAE_SKIP_AUTO_LIBRARY_INITIALIZE=1
+export EXTRAE_CONFIG_FILE=$SRCDIR/extrae.xml
 
 $*
